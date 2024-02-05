6cfcda2fe5d5a6cd962fc8d4880b810e4f94c5a5
Ronit Gaikwad <gaikwadronit@gmail.com>
2024-02-05 11:50:43 +0530

    concat n operators

---
+++ Pavansir_training/Day02/concat.py
@@ -0,0 +1,16 @@
+#concatination  --> its use to join two similar datatype of value +
+
+
+print(10+20) #valid
+print(10.23 +23.32) #valid
+print(12.32+12) #valid
+
+print('weloome'+"Home") #valid concat two string
+print(True+10) #valid True=1
+print(False+1) #valid False=0
+print(True+True)#valid
+print(False+False)#valid
+
+# print(10+"welcome")#False--> not valid bcoz 2 different values
+# print(10.5+"welcome")#False--.>not valid bcoz 2 different values
+# print(True+'welcome')#False-->not valid bcoz 2 different values
---
+++ Pavansir_training/Day02/operators.py
@@ -0,0 +1,36 @@
+
+#Arithmetic operators
+
+a=5
+b=2
+
+print(a+b)#addition-->7
+print (a-b)#subtraction-->3
+print(a*b)#multiplication -->10
+print(a/b)#division-->2.5
+
+print(a//b)#quotient-->2
+print(a%b)#reminder-->1
+print(a**b)# 5**2=25    5*5
+
+#relational operators-->always returns boolean values(T?F)
+# < > != <= >=
+c=2
+d=5
+print(c>d) #False
+print(c<d) #True
+print(c==d)#false
+print(c>=d)#False
+print(c<=d)#True
+print(c!=d)#True
+
+#logical operators-->always returns boolean values
+# and or not
+a=True
+b=False
+
+print(a and b) #False
+print(a or b)   #tRUE
+print(not a) #False
+
+
